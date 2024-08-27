d98c1139cdf229ce44144e3b8e75f3f883b5b573
Woody Ramey <woodyramey@icloud.com>
2024-08-26 23:14:54 -0700

    modify package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/icon"],
   // runtimeConfig: {
   //   public: {
