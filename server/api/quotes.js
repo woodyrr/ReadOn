de050a0370eed2e6a6fd83c2418d1f70f48b5853
Woody Ramey <woodyramey@icloud.com>
2024-08-28 19:28:12 -0700

    quotes.config

--- server/api/quotes.js
+++ server/api/quotes.js
@@ -4,18 +4,19 @@ import { getQuery } from 'h3';
 import chromium from '@sparticuz/chromium';
 // import edgeChromium from 'chrome-aws-lambda'
 // const chromiumPath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
+
+
 export default defineEventHandler(async (event) => {
 
+  let executablePath;
+    if (process.env.VERCEL_ENV === "production") {
+        executablePath = await chromium.executablePath();
+    } else {
+    executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
+    }
 
-
+  
   const { url } = getQuery(event);
-  // let executablePath;
-  // if (process.env.VERCEL_ENV === "production") {
-  //   executablePath = await chromium.executablePath();
-  // } else {
-  //   executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-  // }
-
   console.log("Received URL:", url);  // Log the URL received
   if (!url) {
     return { error: 'No URL provided' };
@@ -23,11 +24,11 @@ export default defineEventHandler(async (event) => {
 // press space tab to bring book back up.
 
 // if (process.env.VERCEL_ENV === "production") {
-  // const executablePath = await chromium.executablePath();
-  const executablePath = 'C:/Program Files/Chromium/Application/chrome.exe' || process.env.CHROMIUM_PATH ;
+//   const executablePath = await chromium.executablePath();
+  
   if (url.includes('https://novelfull.net/')) {
     try {
-        const browser = await puppeteerCore.launch({
+        const browser = await puppeteer.launch({
           // args: chromium.args,
           args: [
             '--disable-gpu',
@@ -48,7 +49,7 @@ export default defineEventHandler(async (event) => {
         const page = await browser.newPage();
         await new Promise(resolve => setTimeout(resolve, 1000)); // 1-second delay
         // await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 10000 });
-        await page.goto(url, { waitUntil: 'networkidle2', timeout: 8000 }); // 10 seconds
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 10000 }); // 10 seconds
         await page.waitForSelector('body');
         const novel = await page.evaluate(() => {
             const chapTitle = document.querySelector('.chapter-title')?.innerText || 'No chapter title';
@@ -70,7 +71,7 @@ export default defineEventHandler(async (event) => {
     
       try {
         // Launch a browser using Puppeteer
-        const browser = await puppeteerCore.launch({
+        const browser = await puppeteer.launch({
           // args: chromium.args,
           args: [
             '--disable-gpu',
@@ -110,316 +111,4 @@ export default defineEventHandler(async (event) => {
         return { error: 'Failed to scrape data' };
       }
     }
-    
-// }
-// else {
-//   const executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-  
-//   if (url.includes('https://novelfull.net/')) {
-//     try {
-//         const browser = await puppeteer.launch({
-//           args: chromium.args,
-//           defaultViewport: chromium.defaultViewport,
-//           executablePath,
-//           headless: false,
-//         });
-//         const page = await browser.newPage();
-//         await page.goto(url, { waitUntil: 'domcontentloaded' });
-
-//         const novel = await page.evaluate(() => {
-//             const chapTitle = document.querySelector('.chapter-title')?.innerText || 'No chapter title';
-//             const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//             const novelText = document.querySelector('.chapter-c')?.innerText || 'No content';
-//             const novelName = document.querySelector('.truyen-title')?.innerText || 'No title';
-//             return { chapTitle, novelName, novelText, nextChap };
-//         });
-//             await browser.close();
-//               // console.log(novel);
-//             return novel;
-//             } 
-//     catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//     }
-//   }
-//   else if (url.includes('https://novelbjn.novelupdates.net/')) {
-//     const executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-//       try {
-//         // Launch a browser using Puppeteer
-//         const browser = await puppeteer.launch({
-//           args: chromium.args,
-//             defaultViewport: chromium.defaultViewport,
-//             executablePath,
-//             headless: chromium.headless,
-//         });
-//         const page = await browser.newPage();
-//         await page.goto(url, { waitUntil: 'domcontentloaded' });
-  
-//         const novel = await page.evaluate(() => {
-//           const chapTitle = document.querySelector('.chr-title')?.innerText || 'No chapter title';
-//           const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//           const novelText = document.querySelector('.chr-c')?.innerText || 'No content';
-//           const novelName = document.querySelector('.novel-title')?.innerText || 'No title';
-          
-//           return { chapTitle, novelName, novelText, nextChap };
-//         });
-        
-//         await browser.close();
-//         // console.log(novel);
-//         return novel;
-//       } catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//       }
-//     }
-    
-// }
-
-// else if (url.includes('https://novelbjn.novelupdates.net/')) {
-//   const executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-  
-//   if (url.includes('https://novelfull.net/')) {
-//     try {
-//         const browser = await puppeteer.launch({
-//           args: chromium.args,
-//           defaultViewport: chromium.defaultViewport,
-//           executablePath,
-//           headless: false,
-//         });
-//         const page = await browser.newPage();
-//         await page.goto(url, { waitUntil: 'domcontentloaded' });
-
-//         const novel = await page.evaluate(() => {
-//             const chapTitle = document.querySelector('.chapter-title')?.innerText || 'No chapter title';
-//             const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//             const novelText = document.querySelector('.chapter-c')?.innerText || 'No content';
-//             const novelName = document.querySelector('.truyen-title')?.innerText || 'No title';
-//             return { chapTitle, novelName, novelText, nextChap };
-//         });
-//             await browser.close();
-//               // console.log(novel);
-//             return novel;
-//             } 
-//     catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//     }
-//   }
-//   else if (url.includes('https://novelbjn.novelupdates.net/')) {
-//     const executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-//       try {
-//         // Launch a browser using Puppeteer
-//         const browser = await puppeteer.launch({
-//           args: chromium.args,
-//             defaultViewport: chromium.defaultViewport,
-//             executablePath,
-//             headless: chromium.headless,
-//         });
-//         const page = await browser.newPage();
-//         await page.goto(url, { waitUntil: 'domcontentloaded' });
-  
-//         const novel = await page.evaluate(() => {
-//           const chapTitle = document.querySelector('.chr-title')?.innerText || 'No chapter title';
-//           const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//           const novelText = document.querySelector('.chr-c')?.innerText || 'No content';
-//           const novelName = document.querySelector('.novel-title')?.innerText || 'No title';
-          
-//           return { chapTitle, novelName, novelText, nextChap };
-//         });
-        
-//         await browser.close();
-//         // console.log(novel);
-//         return novel;
-//       } catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//       }
-//     }
-    
-// }
-
-// else if (url.includes('https://novelbjn.novelupdates.net/')) {
-//   const executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-  
-//   if (url.includes('https://novelfull.net/')) {
-//     try {
-//         const browser = await puppeteer.launch({
-//           args: chromium.args,
-//           defaultViewport: chromium.defaultViewport,
-//           executablePath,
-//           headless: false,
-//         });
-//         const page = await browser.newPage();
-//         await page.goto(url, { waitUntil: 'domcontentloaded' });
-
-//         const novel = await page.evaluate(() => {
-//             const chapTitle = document.querySelector('.chapter-title')?.innerText || 'No chapter title';
-//             const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//             const novelText = document.querySelector('.chapter-c')?.innerText || 'No content';
-//             const novelName = document.querySelector('.truyen-title')?.innerText || 'No title';
-//             return { chapTitle, novelName, novelText, nextChap };
-//         });
-//             await browser.close();
-//               // console.log(novel);
-//             return novel;
-//             } 
-//     catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//     }
-//   }
-//   else if (url.includes('https://novelbjn.novelupdates.net/')) {
-//     const executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-//       try {
-//         // Launch a browser using Puppeteer
-//         const browser = await puppeteer.launch({
-//           args: chromium.args,
-//             defaultViewport: chromium.defaultViewport,
-//             executablePath,
-//             headless: chromium.headless,
-//         });
-//         const page = await browser.newPage();
-//         await page.goto(url, { waitUntil: 'domcontentloaded' });
-  
-//         const novel = await page.evaluate(() => {
-//           const chapTitle = document.querySelector('.chr-title')?.innerText || 'No chapter title';
-//           const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//           const novelText = document.querySelector('.chr-c')?.innerText || 'No content';
-//           const novelName = document.querySelector('.novel-title')?.innerText || 'No title';
-          
-//           return { chapTitle, novelName, novelText, nextChap };
-//         });
-        
-//         await browser.close();
-//         // console.log(novel);
-//         return novel;
-//       } catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//       }
-//     }
-    
-// }
-
-
-
-
-
-
-
-
-// import puppeteerCore from 'puppeteer-core'; // Note: use puppeteer-core
-// import puppeteer from 'puppeteer'; 
-// import { getQuery } from 'h3';
-// import chromium from '@sparticuz/chromium';
-// // import edgeChromium from 'chrome-aws-lambda'
-// // const chromiumPath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-
-
-// export default defineEventHandler(async (event) => {
-
-//   let executablePath;
-//     if (process.env.VERCEL_ENV === "production") {
-//         executablePath = await chromium.executablePath();
-//     } else {
-//         executablePath = process.env.CHROMIUM_PATH || 'C:/Program Files/Chromium/Application/chrome.exe';
-//     }
-
-
-//   const { url } = getQuery(event);
-//   console.log("Received URL:", url);  // Log the URL received
-//   if (!url) {
-//     return { error: 'No URL provided' };
-//   }
-// // press space tab to bring book back up.
-
-// // if (process.env.VERCEL_ENV === "production") {
-// //   const executablePath = await chromium.executablePath();
-  
-//   if (url.includes('https://novelfull.net/')) {
-//     try {
-//         const browser = await puppeteerCore.launch({
-//           // args: chromium.args,
-//           args: [
-//             '--disable-gpu',
-//             '--disable-dev-shm-usage',
-//             '--disable-setuid-sandbox',
-//             '--no-first-run',
-//             '--no-sandbox',
-//             '--no-zygote',
-//             '--deterministic-fetch',
-//             '--disable-features=IsolateOrigins',
-//             '--disable-site-isolation-trials',
-//             // '--single-process',
-//         ],
-//           defaultViewport: chromium.defaultViewport,
-//           executablePath,
-//           headless: chromium.headless,
-//         });
-//         const page = await browser.newPage();
-//         await new Promise(resolve => setTimeout(resolve, 1000)); // 1-second delay
-//         // await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 10000 });
-//         await page.goto(url, { waitUntil: 'networkidle2', timeout: 10000 }); // 10 seconds
-//         await page.waitForSelector('body');
-//         const novel = await page.evaluate(() => {
-//             const chapTitle = document.querySelector('.chapter-title')?.innerText || 'No chapter title';
-//             const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//             const novelText = document.querySelector('.chapter-c')?.innerText || 'No content';
-//             const novelName = document.querySelector('.truyen-title')?.innerText || 'No title';
-//             return { chapTitle, novelName, novelText, nextChap };
-//         });
-//             await browser.close();
-//               // console.log(novel);
-//             return novel;
-//             } 
-//     catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//     }
-//   }
-//   else if (url.includes('https://novelbjn.novelupdates.net/')) {
-    
-//       try {
-//         // Launch a browser using Puppeteer
-//         const browser = await puppeteerCore.launch({
-//           // args: chromium.args,
-//           args: [
-//             '--disable-gpu',
-//             '--disable-dev-shm-usage',
-//             '--disable-setuid-sandbox',
-//             '--no-first-run',
-//             '--no-sandbox',
-//             '--no-zygote',
-//             '--deterministic-fetch',
-//             '--disable-features=IsolateOrigins',
-//             '--disable-site-isolation-trials',
-//             // '--single-process',
-//         ],
-//             defaultViewport: chromium.defaultViewport,
-//             executablePath,
-//             headless: chromium.headless,
-//         });
-//         const page = await browser.newPage();
-//         await new Promise(resolve => setTimeout(resolve, 1000)); // 1-second delay
-//         // await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 10000 });
-//         await page.goto(url, { waitUntil: 'networkidle2', timeout: 10000 }); // 10 seconds
-//         await page.waitForSelector('body');
-//         const novel = await page.evaluate(() => {
-//           const chapTitle = document.querySelector('.chr-title')?.innerText || 'No chapter title';
-//           const nextChap = document.querySelector('.btn-group #next_chap')?.innerText || 'No next chapter link';
-//           const novelText = document.querySelector('.chr-c')?.innerText || 'No content';
-//           const novelName = document.querySelector('.novel-title')?.innerText || 'No title';
-          
-//           return { chapTitle, novelName, novelText, nextChap };
-//         });
-        
-//         await browser.close();
-//         // console.log(novel);
-//         return novel;
-//       } catch (error) {
-//         console.error('Puppeteer failed:', error);
-//         return { error: 'Failed to scrape data' };
-//       }
-//     }
-// });
-
+});
